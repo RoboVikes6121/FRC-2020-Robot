b37ad29d863cd968e230bed9bfb968ab9816af79
Connor Eames <Elitetv2020@gmail.com>
2020-01-08 19:05:58 -0500

    1/8/20  First Push

---
+++ src/main/java/frc/robot/commands/driveManule.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import java.lang.module.ModuleDescriptor.Requires;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+
+public class driveManule extends CommandBase {
+  /**
+   * Creates a new driveManule.
+   */
+  public driveManule() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    Requires(Robot.drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/driveTrain.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class driveTrain extends SubsystemBase {
+  /**
+   * Creates a new ExampleSubsystem.
+   */
+
+  // Creates moter
+  WPI_TalonSRX leftMaster = new WPI_TalonSRX(Constants.left_drive_moter_1);
+  WPI_TalonSRX leftSlave = new WPI_TalonSRX(Constants.left_drive_moter_2);
+  WPI_TalonSRX rightMaster = new WPI_TalonSRX(Constants.right_drive_moter_1);
+  WPI_TalonSRX rightSlave = new WPI_TalonSRX(Constants.right_drive_moter_2);
+
+  // Creates Differential Drive
+  final DifferentialDrive drive = new DifferentialDrive(leftMaster, rightMaster);
+  
+  public driveTrain() {
+      //Sets the slave to follow master
+      leftSlave.follow(leftMaster);
+      rightSlave.follow(rightMaster);
+  }
+
+  public void manualDrive(double move, double turn){
+    if(move > Constants.max_move_speed) move = Constants.max_move_speed;
+    if(move < Constants.min_move_speed) move = Constants.min_move_speed;
+    if(move > Constants.max_move_speed) move = Constants.max_move_speed;
+    if(move < Constants.min_move_speed) move = Constants.min_move_speed;
+
+    drive.arcadeDrive(move, turn);
+  }
+
+  @Override
+  public void periodic() {
+  
+  }
+}
